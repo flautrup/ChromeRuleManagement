75a6f23a5896e4e65811ea33a5a19b282170ea12
flautrup <fredrik@lautrup.se>
2015-02-04 15:10:05 +0100

    Working Angular with CORS/login problem

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
+var server="https://rd-flp2.qliktech.com/";
 
+var service = angular.module("qrsService", ['ngResource']);
+
+service.factory('QRS', function ($resource){
+  return $resource(server+'qrs/about');
+});
+
+/*service.factory("QRS", function () {
+  var QRS = function() {
+    this.get = function() {
+      return ("FactoryResults");
+    };
+  };
+});*/
+
+ service.controller("qrsController", ["$scope","QRS", function($scope, QRS) {
+ var about=QRS.get(function() {
+   console.log(about);
+ });
+
+ var login=function() {
+   $http.get("https://rd-flp2.qliktech.com:4244/windows_authentication/", { withCredentials: true });
+ }
+
+ $scope.about=about;
+}]);
