dc505e36c4c6068b268b2d6ea2217c8c17d35f49
flautrup <fredrik@lautrup.se>
2015-02-05 14:36:18 +0100

    QRS requests working
    Able to login to QRS, request list of rules and get details of a rule.

--- main.js
+++ main.js
@@ -1,35 +1,46 @@
 var server="https://rd-flp2.qliktech.com/";
+var xrfkey=rand(16);
 
 var service = angular.module("qrsService", ['ngResource']);
 
+function rand(length,current){
+ current = current ? current : '';
+ return length ? rand( --length , "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz".charAt( Math.floor( Math.random() * 60 ) ) + current ) : current;
+}
+
+service.config(["$httpProvider", function($httpProvider) {
+  $httpProvider.defaults.headers.common = { 'x-qlik-xrfkey': xrfkey };
+}]);
+
 service.factory('QRS', function ($resource){
-  return $resource(server+'qrs/about');
+  return $resource(server+'qrs/systemrule/:ruleId?xrfkey='+xrfkey);
 });
 
-/*service.factory("QRS", function () {
-  var QRS = function() {
-    this.get = function() {
-      return ("FactoryResults");
-    };
-  };
-});*/
 
- service.controller("qrsController", ["$scope","$http", "QRS", function($scope, $http, QRS) {
+service.controller("qrsController", ["$scope","$http",  "QRS", function($scope, $http, QRS) {
 
+ $scope.logedin="Logged out";
  $scope.about="Empty";
 
- $scope.refresh=function() {
-   /*var about=QRS.get(function() {
-      console.log(about);
-    });*/
+ $scope.list=function() {
+   $scope.rulelist=QRS.query(function() {
+      console.log($scope.rulelist);
+   });
 
-  $scope.about="Updated";
+ };
+
+ $scope.detail=function(id) {
+   $scope.ruledetail=QRS.get({ruleId: id}, function() {
+      console.log($scope.ruledetail);
+   });
+
  };
 
 
 $scope.login=function() {
-   $http.get("https://rd-flp2.qliktech.com/qrs/about", { withCredentials: true });
- }
 
+   $http.get("https://rd-flp2.qliktech.com/qrs/about?xrfkey="+xrfkey, { withCredentials: true });
+   $scope.logedin="Logged in"
+ }
 
 }]);
