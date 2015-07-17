5266c7a187e3a67a441a47660e3efffc2a8de348
flautrup <fredrik@lautrup.se>
2015-07-17 15:57:55 +0200

    UI changes
    New icons and better layout

--- script/controllers/qrsController.js
+++ script/controllers/qrsController.js
@@ -10,7 +10,7 @@ service.controller("qrsController", ["$scope","$http",  "qrsRules", "qrsCustProp
     ruleList: []
   };
 
-//Get the list of rules, parse for custom properties and fetch the custom properies
+//Get the list of rules, parse for custom properties and fetch the custom properies and store with rule
  $scope.list=function() {
    //GET rules
    serverRuleList=qrsRules.query(function() {
@@ -49,6 +49,7 @@ service.controller("qrsController", ["$scope","$http",  "qrsRules", "qrsCustProp
 
 };
 
+//Store custom property in rule
 $scope.storeCustomPropertyInRule=function(index,customPropName) {
    var tmpCustObj=qrsCustProp.get({custPropName: customPropName});
    //Connect custom property definitions to rules that use them
@@ -58,14 +59,26 @@ $scope.storeCustomPropertyInRule=function(index,customPropName) {
     });
 };
 
+//Change in rule and not added atribute.
+$scope.toogleRule = function(rule) {
+  var count=0;
+  while(rule.id!=serverRuleList[count].id) {
+    count++;
+  }
+  serverRuleList[count].disabled=!serverRuleList[count].disabled;
+}
+
+//Add rule to rule package
 $scope.addToRulePackage=function (rule) {
    $scope.rulePackageObj=rulePackage.add(rule);
 };
 
+//Set rulepackage name
 $scope.setRulePackageName = function (name) {
   return rulePackage.setName(name);
 };
 
+//Set rulepackage description
 $scope.setRulePackageDescription = function (description) {
   return rulePackage.setDescription(description);
 };
@@ -93,25 +106,30 @@ $scope.loadRulePackage = function (findRulePackage) {
   }
 };
 
+//Upload rulepackage to server
 $scope.uploadRulePackage = function (findRuleName) {
 };
 
+//Delete rulepackage
 $scope.deleteRulePackage = function (findRuleName) {
   $scope.packageList=localStorage.delete(findRuleName);
 }
-
-$scope.importToServer=function (rule) {
-
-};
 
 $scope.detail=function(id) {
    $scope.ruleDetail=qrsRule.get({ruleId: id}, function() {
       console.log($scope.ruleDetail);
    });
-
 };
 
+$scope.disableRow=function(rule) {
+  if (rule.disabled) {
+    return "core-item ruledissabled";
+  } else {
+    return "core-item";
+  }
+}
 
+//Login to server
 $scope.login=function() {
 
    $http.get($scope.server+"/hub?xrfkey="+XRFKEY, { withCredentials: true });
