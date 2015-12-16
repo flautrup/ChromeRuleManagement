ac58681a1e00d1475202e4af3fe3fc76a47f18f3
flautrup <fredrik.lautrup@qlik.com>
2015-12-16 09:47:47 +0100

    Styling and adaption

--- main.js
+++ main.js
@@ -1,28 +1,34 @@
 //Configuration
-var SERVER="https://rd-flp2.qliktech.com/";
-var XRFKEY=rand(16);
+var SERVER = "https://rd-flp2.qliktech.com/";
+var XRFKEY = rand(16);
 
 
 // Event handling
 var tabs = document.querySelector('md-tabs');
 var pages = document.querySelector('core-pages');
 
-tabs.addEventListener('core-select',function(){
+tabs.addEventListener('core-select', function() {
   pages.selected = tabs.selected;
 });
 
 
 //Supporting functions
-function rand(length,current){
- current = current ? current : '';
- return length ? rand( --length , "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz".charAt( Math.floor( Math.random() * 60 ) ) + current ) : current;
+function rand(length, current) {
+  current = current ? current : '';
+  return length ? rand(--length, "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz".charAt(Math.floor(Math.random() * 60)) + current) : current;
 }
 
 //Angular code
 
 //Services
-var service = angular.module("qrsService", ['ngResource','ngMaterial']);
+var service = angular.module("qrsService", ['ngResource', 'ngMaterial']).config(function($mdThemingProvider) {
+  $mdThemingProvider.theme('default')
+    .primaryPalette('green',{'default': '800'})
+    .accentPalette('orange');
+});;
 
 service.config(["$httpProvider", function($httpProvider) {
-  $httpProvider.defaults.headers.common = { 'x-qlik-xrfkey': XRFKEY };
+  $httpProvider.defaults.headers.common = {
+    'x-qlik-xrfkey': XRFKEY
+  };
 }]);
