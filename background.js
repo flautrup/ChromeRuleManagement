abf0399e1de2e26fd46c78de34344e64fbb92db8
flautrup <fredrik.lautrup@qlik.com>
2016-01-21 13:41:29 +0100

    Fixed size window

--- background.js
+++ background.js
@@ -5,11 +5,21 @@
  * @see http://developer.chrome.com/apps/app.window.html
  */
 chrome.app.runtime.onLaunched.addListener(function(launchData) {
+  var windowWidth = 1024;
+  var windowHeight = 700;
+
   chrome.app.window.create(
-    'index.html',
-    {
-      id: 'mainWindow',
-      bounds: {width: 800, height: 600}
+    'index.html', {
+      //id: 'mainWindow',
+      outerBounds: { // 'bounds' is deprecated, and you want full window size
+        width: windowWidth,
+        height: windowHeight
+      },
+      resizable: false,
+      frame: 'chrome'
     }
   );
+
+
+
 });
